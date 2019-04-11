ae1657471f3d0b5b4d161f38fc75777bf55dea52
Joice Gomes <joice.gomes@sap.com>
2019-04-10 21:56:35 -0300

    Change the data represantion

--- city.py
+++ city.py
@@ -1,20 +1,33 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Apr  8 19:53:35 2019
-
-@author: I502765
-"""
 import math
 
-class City:
-    def __init__(self, id, x, y):
-        self.id = id
+class Vertice:
+    def __init__(self, x, y):
         self.x = x
-        self.y = y
+        self.y = y   
 
-    def get_distance(self, other_city):
-        distance_x = abs(float(self.x) - float(other_city.x)) ** 2
-        distance_y = abs(float(self.y) - float(other_city.y)) ** 2
+class City:    
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.vertices_quant = len(vertices)
 
-        return math.sqrt(distance_x + distance_y)
+    def get_x(self, vertice_id):
+        return self.vertices[vertice_id][0]
+
+    def get_y(self, vertice_id):
+        return self.vertices[vertice_id][1]
+        
+    _distances = {}
+
+    def get_distance(self, f_city, s_city):
+        if (f_city, s_city) in self._distances:
+            return self._distances[(f_city, s_city)]
+
+        distance_x = abs(float(f_city.get_x) - float(s_city.get_x)) ** 2
+        distance_y = abs(float(f_city.get_y) - float(s_city.get_y)) ** 2
+        _distance = math.sqrt(distance_x + distance_y)
+        print(_distance)
+
+        self._distances[(f_city, s_city)], self._distances[(s_city, f_city)] = _distance, _distance
+        return _distance
 
